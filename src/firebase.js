807a0fe048df169e43e80031265a89c8ba31fc24
aim9886 <aimanth9886@gmail.com>
2024-01-16 21:23:43 +0530

    second commit

--- src/firebase.js
+++ src/firebase.js
@@ -9,13 +9,6 @@ const firebaseApp = firebase.initializeApp({
   storageBucket: "real-chat-app-b79ea.appspot.com",
   messagingSenderId: "125550149927",
   appId: "1:125550149927:web:71093ab7daa469fe33a902"
-  // apiKey: "AIzaSyDEqPQY3u_ndUnWmUAAm0xiSJGobGU0R_Y",
-  // authDomain: "firechat-85ff2.firebaseapp.com",
-  // projectId: "firechat-85ff2",
-  // storageBucket: "firechat-85ff2.appspot.com",
-  // messagingSenderId: "578938903049",
-  // appId: "1:578938903049:web:93e7013707585e9c7176d8",
-  // measurementId: "G-M8TJ5R1CTY"
 })
 
 const db = firebaseApp.firestore()
